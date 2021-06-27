79a2d6838deb5650f4d6342e435b56a9a2abf91b
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-06-27 16:22:22 +0200

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -1,7 +1,7 @@
 (function()  {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
-        <h1>Hello World</h1>
+        <h1>Space Invaders</h1>
     `;
 
     customElements.define('com-sap-sample-helloworld1', class HelloWorld1 extends HTMLElement {
@@ -12,7 +12,30 @@
 			this._shadowRoot = this.attachShadow({mode: "open"});
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
             this._firstConnection = false;
-		}
+            const grid = document.querySelector('.grid')
+            for (let i = 0; i < 225; i++) {
+                const square = document.createElement('div')
+                  grid.appendChild(square)
+                }
+
+            const squares = Array.from(document.querySelectorAll('.grid div'))
+
+            const alienInvaders = [
+            0,1,2,3,4,5,6,7,8,9,
+            15,16,17,18,19,20,21,22,23,24,
+            30,31,32,33,34,35,36,37,38,39
+          ]
+
+          function draw() {
+            for (let i = 0; i < alienInvaders.length; i++) {
+              if(!aliensRemoved.includes(i)) {
+                squares[alienInvaders[i]].classList.add('invader')
+              }
+            }
+          }
+
+          draw()
+          		}
 
         //Fired when the widget is added to the html DOM of the page
         connectedCallback(){
