931ad9ff5cc620dcb004771424f6d1dc9aeb39d4
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-06-28 21:54:15 +0200

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -44,11 +44,7 @@
 			this._shadowRoot = this.attachShadow({mode: "open"});
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
             this._firstConnection = false;
-	    var this._grid = this._shadowRoot.querySelector('.grid')
-	    for (let i = 0; i < 225; i++) {
-  		var this._square = this._shadowRoot.createElement('div')
- 		this._grid.appendChild(this._square)
-		}
+	    const grid = document.querySelector('.grid')
             		}
 
         //Fired when the widget is added to the html DOM of the page
