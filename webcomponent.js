06c1dc6f93774147a52ec82e3c35253f4227b070
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-06-29 20:51:11 +0200

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -70,7 +70,20 @@
             }    
         
         squares[currentShooterIndex].classList.add('shooter')
-            
+        
+        function moveShooter(e) {
+            squares[currentShooterIndex].classList.remove('shooter')
+            switch(e.key) {
+              case 'ArrowLeft':
+                if (currentShooterIndex % width !== 0) currentShooterIndex -=1
+                break
+              case 'ArrowRight' :
+                if (currentShooterIndex % width < width -1) currentShooterIndex +=1
+                break
+            }
+            squares[currentShooterIndex].classList.add('shooter')
+          }
+
         }
 	    
         //Fired when the widget is added to the html DOM of the page
