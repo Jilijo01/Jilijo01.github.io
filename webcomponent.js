c0956eae99da5d8d350ec689944a65e6708b7ce0
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-06-29 06:40:32 +0200

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -57,7 +57,7 @@
 	    15,16,17,18,19,20,21,22,23,24,
 	    30,31,32,33,34,35,36,37,38,39]
             		}
-	    function draw() {
+	    function draw_SPEC() {
   		for (let i = 0; i < alienInvaders.length; i++) {
     			if(!aliensRemoved.includes(i)) {
      				 squares[alienInvaders[i]].classList.add('invader')
