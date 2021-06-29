e263e1508f21c7e76a8e7d1d95e65c68c2e8a02f
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-06-29 06:51:13 +0200

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -57,15 +57,15 @@
 	    15,16,17,18,19,20,21,22,23,24,
 	    30,31,32,33,34,35,36,37,38,39]
             		}
-	    function testing() {
+	    //function testing() {
   		for (let i = 0; i < alienInvaders.length; i++) {
     			if(!aliensRemoved.includes(i)) {
      				 squares[alienInvaders[i]].classList.add('invader')
 			    }
 			  }
-			}
+			//}
 
-	    testing()
+	    //testing()
         //Fired when the widget is added to the html DOM of the page
         connectedCallback(){
             this._firstConnection = true;
