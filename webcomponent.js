974c878c6aac3014e86b579c3912b8e49297316d
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-06-30 21:30:39 +0200

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -111,20 +111,20 @@
         */
         
         redraw(){
-	    let currentShooterIndex = 202
-	    let width = 15
-	    const squares = Array.from(this._shadowRoot.querySelectorAll('.grid div'))
+	let currentShooterIndex = 202
+	let width = 15
+	const squares = Array.from(this._shadowRoot.querySelectorAll('.grid div'))
         var shadow = window.getSelection(this._shadowRoot);
         document.addEventListener('keydown', logKey);
 	    function logKey(e) {
 		    squares[currentShooterIndex].classList.remove('shooter');
-	            switch(e.key) {
-                  case 'ArrowLeft':
-                    if (currentShooterIndex % width !== 0) currentShooterIndex -=1
-                    break
-                  case 'ArrowRight' :
-                    if (currentShooterIndex % width < width -1) currentShooterIndex +=1
-                    break
+		    switch(e.key) {
+		  case 'ArrowLeft':
+		    if (currentShooterIndex % width !== 0) currentShooterIndex -=1
+		    break
+		  case 'ArrowRight' :
+		    if (currentShooterIndex % width < width -1) currentShooterIndex +=1
+		    break
                 }
                 squares[currentShooterIndex].classList.add('shooter');
               }
