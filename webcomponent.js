9b315cc30a2e488ff6f3270060d230ec23afdab9
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-06-30 21:42:41 +0200

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -111,6 +111,7 @@
         */
         
         redraw(){
+	    let alienInvaders = [0,1,2,3,4,5,6,7,8,9,15,16,17,18,19,20,21,22,23,24,30,31,32,33,34,35,36,37,38,39]
 	    let currentShooterIndex = 202
 	    let width = 15
 	    const squares = Array.from(this._shadowRoot.querySelectorAll('.grid div'))
