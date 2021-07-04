50cdcde76a95d8275c17e47d2e63f78f6f8d0cc4
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-07-04 09:11:57 +0200

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -1,28 +1,78 @@
 (function()  {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
-    <head>
-        <title>Super Mario</title>
-        <script type="module" src="https://jilijo01.github.io/webcomponent.js"></script>
-    </head>
-    <body>
-    <canvas id="screen" width="640" height="640"></script>
-    </body>
-        
-
+        <h1>Space Invaders</h1>
+	<div class="grid"></div>
+	<style>
+	.grid {
+	width: 300px;
+	height: 300px;
+	border: solid black 1px;
+	display: flex;
+	flex-wrap: wrap;
+	}
+	.grid div {
+		width: 20px;
+		height: 20px;
+	}
+	.invader {
+		background-color: purple;
+		border-radius: 10px;
+	}
+	.shooter {
+		background-color: green;
+	}
+	.laser {
+		background-color: orange;
+	}
+	.boom {
+		background-color: red;
+	}
+	</style>
     `;
 
     customElements.define('com-sap-sample-helloworld1', class HelloWorld1 extends HTMLElement {
 
 
-		constructor() {
-			super(); 
-			this._shadowRoot = this.attachShadow({mode: "open"});
-            this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
-            this._firstConnection = false;
-			
-	}
+	    constructor() {
+		super();
+	    this._shadowRoot = this.attachShadow({mode: "open"});
+        this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+        this._firstConnection = false;
+        
+        let aliensRemoved = []
+        let currentShooterIndex = 202
+	    const grid = document.querySelector('.grid');
+	    for (let i = 0; i < 225; i++) {
+	   	const square = document.createElement('div');
+	    	this._shadowRoot.querySelector('.grid').appendChild(square);
+		}
+	    const squares = Array.from(this._shadowRoot.querySelectorAll('.grid div'))
 
+	    let alienInvaders = [
+	    0,1,2,3,4,5,6,7,8,9,
+	    15,16,17,18,19,20,21,22,23,24,
+	    30,31,32,33,34,35,36,37,38,39]
+            		
+	    function draw() {
+  		for (let i = 0; i < alienInvaders.length; i++) {
+    			if(!aliensRemoved.includes(i)) {
+     				 squares[alienInvaders[i]].classList.add('invader')
+			    }
+			  }
+			}
+            draw()
+        
+        function remove() {
+            for (let i = 0; i < alienInvaders.length; i++) {
+                squares[alienInvaders[i]].classList.remove('invader')
+            }
+            }    
+        
+        squares[currentShooterIndex].classList.add('shooter')
+       
+        }
+	    
         //Fired when the widget is added to the html DOM of the page
         connectedCallback(){
             this._firstConnection = true;
@@ -31,7 +81,7 @@
 
          //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
         disconnectedCallback(){
-        
+
         }
 
          //When the custom widget is updated, the Custom Widget SDK framework executes this function first
@@ -45,12 +95,12 @@
                 this.redraw();
             }
         }
-        
+
         //When the custom widget is removed from the canvas or the analytic application is closed
         onCustomWidgetDestroy(){
         }
 
-        
+
         //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
         // Commented out by default.  If it is enabled, SAP Analytics Cloud will track DOM size changes and call this callback as needed
         //  If you don't need to react to resizes, you can save CPU by leaving it uncommented.
@@ -59,8 +109,141 @@
             redraw()
         }
         */
-
+        
         redraw(){
+	    const grid = document.querySelector('.grid')
+        const resultsDisplay = document.querySelector('.results')
+        let currentShooterIndex = 202
+        let width = 15
+        let direction = 1
+        let invadersId
+        let goingRight = true
+        let aliensRemoved = []
+        let results = 0
+
+        for (let i = 0; i < 225; i++) {
+        const square = document.createElement('div')
+        this._shadowRoot.appendChild(square)
+        }
+
+        const squares = Array.from(this._shadowRoot.querySelectorAll('.grid div'))
+
+        const alienInvaders = [
+        0,1,2,3,4,5,6,7,8,9,
+        15,16,17,18,19,20,21,22,23,24,
+        30,31,32,33,34,35,36,37,38,39
+        ]
+
+        function draw() {
+        for (let i = 0; i < alienInvaders.length; i++) {
+            if(!aliensRemoved.includes(i)) {
+            squares[alienInvaders[i]].classList.add('invader')
+            }
+        }
+        }
+
+        draw()
+
+        function remove() {
+        for (let i = 0; i < alienInvaders.length; i++) {
+            squares[alienInvaders[i]].classList.remove('invader')
+        }
+        }
+
+        squares[currentShooterIndex].classList.add('shooter')
+
+
+        function moveShooter(e) {
+        squares[currentShooterIndex].classList.remove('shooter')
+        switch(e.key) {
+            case 'ArrowLeft':
+            if (currentShooterIndex % width !== 0) currentShooterIndex -=1
+            break
+            case 'ArrowRight' :
+            if (currentShooterIndex % width < width -1) currentShooterIndex +=1
+            break
+        }
+        squares[currentShooterIndex].classList.add('shooter')
+        }
+        document.addEventListener('keydown', moveShooter)
+
+        function moveInvaders() {
+        const leftEdge = alienInvaders[0] % width === 0
+        const rightEdge = alienInvaders[alienInvaders.length - 1] % width === width -1
+        remove()
+
+        if (rightEdge && goingRight) {
+            for (let i = 0; i < alienInvaders.length; i++) {
+            alienInvaders[i] += width +1
+            direction = -1
+            goingRight = false
+            }
+        }
+
+        if(leftEdge && !goingRight) {
+            for (let i = 0; i < alienInvaders.length; i++) {
+            alienInvaders[i] += width -1
+            direction = 1
+            goingRight = true
+            }
+        }
+
+        for (let i = 0; i < alienInvaders.length; i++) {
+            alienInvaders[i] += direction
+        }
+
+        draw()
+
+        if (squares[currentShooterIndex].classList.contains('invader', 'shooter')) {
+            resultsDisplay.innerHTML = 'GAME OVER'
+            clearInterval(invadersId)
+        }
+
+        for (let i = 0; i < alienInvaders.length; i++) {
+            if(alienInvaders[i] > (squares.length)) {
+            resultsDisplay.innerHTML = 'GAME OVER'
+            clearInterval(invadersId)
+            }
+        }
+        if (aliensRemoved.length === alienInvaders.length) {
+            resultsDisplay.innerHTML = 'YOU WIN'
+            clearInterval(invadersId)
+        }
+        }
+        invadersId = setInterval(moveInvaders, 600)
+
+        function shoot(e) {
+        let laserId
+        let currentLaserIndex = currentShooterIndex
+        function moveLaser() {
+            squares[currentLaserIndex].classList.remove('laser')
+            currentLaserIndex -= width
+            squares[currentLaserIndex].classList.add('laser')
+
+            if (squares[currentLaserIndex].classList.contains('invader')) {
+            squares[currentLaserIndex].classList.remove('laser')
+            squares[currentLaserIndex].classList.remove('invader')
+            squares[currentLaserIndex].classList.add('boom')
+
+            setTimeout(()=> squares[currentLaserIndex].classList.remove('boom'), 300)
+            clearInterval(laserId)
+
+            const alienRemoved = alienInvaders.indexOf(currentLaserIndex)
+            aliensRemoved.push(alienRemoved)
+            results++
+            //resultsDisplay.tmpl.innerHTML = results
+            console.log(aliensRemoved)
+            }
+
+        }
+        switch(e.key) {
+            case 'ArrowUp':
+            laserId = setInterval(moveLaser, 100)
+        }
+        }
+
+        document.addEventListener('keydown', shoot)
+
         }
     });
 })();
