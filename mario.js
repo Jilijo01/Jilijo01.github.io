fc6c7fffa3cdb4ff06f809e5eb75240c8b716e8b
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-07-05 06:49:19 +0200

    Update mario.js

--- mario.js
+++ mario.js
@@ -15,9 +15,16 @@
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
             this._firstConnection = false;
             var canvas = this._shadowRoot.getElementById('screen').getContext('2d');
-            console.log(canvas);
-            //var context = canvas.fillRect(0,0,50,50);
-            //context.fillRect(0,0,50,50);
+            function loadImage(url) {
+                return new Promise(resolve => {
+                    const image = new Image();
+                    image.addEventListener('load', () => {
+                        resolve(image);
+                    });
+                    image.src = url;
+                });
+            }
+            loadImage('https://jilijo01.github.io/tiles.png');
         }
 
         //Fired when the widget is added to the html DOM of the page
