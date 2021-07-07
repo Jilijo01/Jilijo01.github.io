8b65c4cedfe646e665885f8bbc1c3e6466fcdf0b
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-07-07 06:24:40 +0200

    Update mario.js

--- mario.js
+++ mario.js
@@ -14,7 +14,7 @@
             this._shadowRoot = this.attachShadow({ mode: "open" });
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
             this._firstConnection = false;
-            //var context = this._shadowRoot.getElementById('screen').getContext('2d');
+            var context = this._shadowRoot.getElementById('screen').getContext('2d');
             function loadImage(url) {
                 return new Promise(resolve => {
                     const image = new Image();
@@ -62,7 +62,7 @@
             .then(image => {
                 const sprites = new SpriteSheet(image,16,16);
                 sprites.define('ground',0, 0);
-                //sprites.draw('ground', context, 45, 62);
+                sprites.draw('ground', context, 45, 62);
                 //context.drawImage(image,0,0,16,16,0,0,16,16);
             });
         }
