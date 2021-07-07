e56151e57a995d5a504a6e6be81b8cce49042dad
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-07-07 06:20:46 +0200

    Update mario.js

--- mario.js
+++ mario.js
@@ -26,36 +26,36 @@
             }
 
             class SpriteSheet {
-                constructor(image, width, height){
-                this.image = image;
-                this.width = width;
-                this.height = height;
-                this.tiles = new Map(); 
-                }
-                
-                define (name, x, y){
-                    const buffer= document.createElement('canvas');
-                    buffer.width = this.width;
-                    buffer.height = this.height;
-                    buffer
-                        .getContext('2d')
-                        .drawImage(
-                            this.image,
-                            x * this.width,
-                            y * this.height,
-                            this.width,
-                            this.height,
-                            0,
-                            0,
-                            this.width,
-                            this.height);
-                    this.tiles.set(name,buffer);    
-                }
+    constructor(image, w = 16, h = 16) {
+        this.image = image;
+        this.width = w;
+        this.height = h;
+        this.tiles = new Map();
+    }
 
-                draw(name, context, x, y){
-                    const buffer = this.tiles.get(name);
-                    context.drawImage(buffer, x, y);
-                }
+    define(name, x, y) {
+        const buffer = document.createElement('canvas');
+        buffer.height = this.height;
+        buffer.width = this.width;
+        buffer
+            .getContext('2d')
+            .drawImage(
+                this.image,
+                this.width * x,
+                this.height * y,
+                this.width,
+                this.height,
+                0,
+                0,
+                this.width,
+                this.height);
+        this.tiles.set(name, buffer);
+    }
+
+    draw(name, context, x, y) {
+        const buffer = this.tiles.get(name);
+        context.drawImage(buffer, x, y);
+    }
             }
 
             loadImage('https://jilijo01.github.io/tiles.png')
