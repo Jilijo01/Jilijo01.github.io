cffbe4585c5427fe3f99da457850fc8e4b28a3d9
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-07-15 14:18:51 +0200

    Update mario.js

--- mario.js
+++ mario.js
@@ -68,7 +68,9 @@
                     sprites.define('ground', 0, 0);
                     sprites.define('sky', 3, 23);
                     sprites.draw('sky', context, 180, 162);
-                    console.log(x);
+                    for (let x = 0, x < 25, ++x){
+                        console.log(x);
+                        };
                     console.log(y);
                     /*for (let x = 0, x < 25, ++x){
                         for (let y = 0, y < 25, ++y){
