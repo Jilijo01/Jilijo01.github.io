2c6109f9bfa957424206d3bc78b69511b92fddf2
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-07-15 18:51:26 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 import SpriteSheet from 'https://jilijo01.github.io/SpriteSheet.js';
 import {loadImage, loadLevel} from 'https://jilijo01.github.io/loaders.js';
-this._shadowRoot = this.attachShadow({ mode: "open" });
-this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
-this._firstConnection = false;
-const context = this.shadowRoot.getElementById('screen').getContext('2d');
+const context = shadow-root.getElementById('screen').getContext('2d');
 
 function drawBackground(background, context, sprites) {
     background.ranges.forEach(([x1, x2, y1, y2]) => {
