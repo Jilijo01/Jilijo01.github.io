46451181b3ef52da2cad4ef4bf47eb7ca87ea21f
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-07-15 09:37:18 +0200

    Update mario.js

--- mario.js
+++ mario.js
@@ -69,7 +69,7 @@
                     sprites.define('sky', 3, 23);
                     sprites.draw('sky', context, 180, 162);
 
-                    for (let x = 0, x < 25, ++x){
+                    for (let a = 0, a < 25, ++a){
                         for (let y = 0, y < 25, ++y){
                             sprites.drawTile('sky', context, x, y)
                         }
