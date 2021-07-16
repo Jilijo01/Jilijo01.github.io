0ffdf21ae574044069a692165e0781f4587a0257
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-07-16 15:26:26 +0200

    Update mario.js

--- mario.js
+++ mario.js
@@ -18,6 +18,26 @@
             this.newScript.setAttribute('type', 'module');
             this.newScript.setAttribute('src', 'https://jilijo01.github.io/main.js');
             this._shadowRoot.appendChild(this.newScript);
+           var context = this._shadowRoot.getElementById('screen').getContext('2d');
+            
+            loadImage('https://jilijo01.github.io/tiles.png')
+                .then(image => {
+                    const sprites = new SpriteSheet(image, 16, 16);
+                    sprites.define('ground', 0, 0);
+                    sprites.define('sky', 3, 23);
+                    sprites.draw('sky', context, 180, 162);
+                    for (let x = 0; x < 25; ++x) {
+                        for (let y = 0; y < 14; ++y) {
+                            sprites.drawTile('sky', context, x, y);
+                        }
+                    }
+                    for (let x = 0; x < 25; ++x) {
+                        for (let y = 12; y < 14; ++y) {
+                            sprites.drawTile('ground', context, x, y);
+                        }
+                    }
+                });
+        }
         }
 
         //Fired when the widget is added to the html DOM of the page
