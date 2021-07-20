41a30d3bd8d50520646c0413a3efbdf4c1f67854
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-07-20 09:28:43 +0200

    Update mario.js

--- mario.js
+++ mario.js
@@ -1,7 +1,7 @@
 (function () {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
-    <h1>Super Mario</h1>
+    <h1>Testing</h1>
     <canvas id="screen" width="640" height="640"></canvas>
     
     `;
