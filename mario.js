caceb97f10e4aac5b304485096d3723d82a2692c
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-07-24 08:15:27 +0200

    Update mario.js

--- mario.js
+++ mario.js
@@ -62,13 +62,13 @@
                 }
             };
 
-            function loadLevel(name){
+            function loadLevel(name) {
                 return fetch(`https://jilijo01.github.io/${name}.json`)
                     .then(r => r.json());
             }
 
-            function drawBackground(background, context, sprites){
-                background.ranges.forEach(([x1,x2,y1,y2]) => {
+            function drawBackground(background, context, sprites) {
+                background.ranges.forEach(([x1, x2, y1, y2]) => {
                     for (let x = x1; x < x2; ++x) {
                         for (let y = y1; y < y2; ++y) {
                             sprites.drawTile(background.tile, context, x, y);
@@ -77,61 +77,67 @@
                 });
             }
 
-            loadImage('https://jilijo01.github.io/tiles.png')
-                .then(image => {
-                    const sprites = new SpriteSheet(image, 16, 16);
-                    sprites.define('ground', 0, 0);
-                    sprites.define('sky', 3, 23);
-                    sprites.draw('sky', context, 180, 162);
+            function loadBackgroundSprites() {
+                return loadImage('https://jilijo01.github.io/tiles.png')
+                    .then(image => {
+                        const sprites = new SpriteSheet(image, 16, 16);
+                        sprites.define('ground', 0, 0);
+                        sprites.define('sky', 3, 23);
+                        sprites.draw('sky', context, 180, 162);
+                        return sprites;
+                    }
 
-                    loadLevel('1-1')
-                        .then(level => {
-                            level.backgrounds.forEach(background =>{
-                                drawBackground(background,context,sprites);
-                            })
-                            
-                        });
-                });
-        }
+            Promise.all([
+                        loadBackgroundSprites(),
+                        loadLevel('1-1')
+                    ])
+                            .then(([sprites, level]) => {
+                                level.background.forEach(background => {
+                                    drawBackground(background, context, sprites);
+                                })
+
+                            });
+            });
+}
 
         //Fired when the widget is added to the html DOM of the page
         connectedCallback() {
-            this._firstConnection = true;
-            this.redraw();
-        }
+    this._firstConnection = true;
+    this.redraw();
+}
 
-        //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
-        disconnectedCallback() {
+//Fired when the widget is removed from the html DOM of the page (e.g. by hide)
+disconnectedCallback() {
 
-        }
+}
 
-        //When the custom widget is updated, the Custom Widget SDK framework executes this function first
-        onCustomWidgetBeforeUpdate(oChangedProperties) {
+//When the custom widget is updated, the Custom Widget SDK framework executes this function first
+onCustomWidgetBeforeUpdate(oChangedProperties) {
 
-        }
+}
 
-        //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
-        onCustomWidgetAfterUpdate(oChangedProperties) {
-            if (this._firstConnection) {
-                this.redraw();
-            }
-        }
+//When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
+onCustomWidgetAfterUpdate(oChangedProperties) {
+    if (this._firstConnection) {
+        this.redraw();
+    }
+}
 
-        //When the custom widget is removed from the canvas or the analytic application is closed
-        onCustomWidgetDestroy() {
-        }
+//When the custom widget is removed from the canvas or the analytic application is closed
+onCustomWidgetDestroy() {
+}
 
 
-        //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
-        // Commented out by default.  If it is enabled, SAP Analytics Cloud will track DOM size changes and call this callback as needed
-        //  If you don't need to react to resizes, you can save CPU by leaving it uncommented.
-        /*
-        onCustomWidgetResize(width, height){
-            redraw()
-        }
-        */
+//When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
+// Commented out by default.  If it is enabled, SAP Analytics Cloud will track DOM size changes and call this callback as needed
+//  If you don't need to react to resizes, you can save CPU by leaving it uncommented.
+/*
+onCustomWidgetResize(width, height){
+    redraw()
+}
+*/
 
-        redraw() {
-        }
+redraw() {
+}
     });
-})();
+}) ();
