845b513b9df41b9aa7c3218a4a731eee1d99cb27
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-07-24 06:52:05 +0200

    Update mario.js

--- mario.js
+++ mario.js
@@ -1,9 +1,9 @@
 (function () {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
-    <h1>Testing</h1>
+    <h1>Super Mario</h1>
     <canvas id="screen" width="640" height="640"></canvas>
-    
+        
     `;
 
     customElements.define('com-sap-sample-helloworld1', class HelloWorld1 extends HTMLElement {
@@ -14,13 +14,72 @@
             this._shadowRoot = this.attachShadow({ mode: "open" });
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
             this._firstConnection = false;
-            this.newScript = document.createElement('script');
-            this.newScript.setAttribute('type', 'module');
-            this.newScript.setAttribute('src', 'https://jilijo01.github.io/main.js');
-            this._shadowRoot.appendChild(this.newScript);
             var context = this._shadowRoot.getElementById('screen').getContext('2d');
+            function loadImage(url) {
+                return new Promise(resolve => {
+                    const image = new Image();
+                    image.addEventListener('load', () => {
+                        resolve(image);
+                    });
+                    image.src = url;
+                });
+            }
+
+            class SpriteSheet {
+                constructor(image, w = 16, h = 16) {
+                    this.image = image;
+                    this.width = w;
+                    this.height = h;
+                    this.tiles = new Map();
+                }
+
+                define(name, x, y) {
+                    const buffer = document.createElement('canvas');
+                    buffer.height = this.height;
+                    buffer.width = this.width;
+                    buffer
+                        .getContext('2d')
+                        .drawImage(
+                            this.image,
+                            this.width * x,
+                            this.height * y,
+                            this.width,
+                            this.height,
+                            0,
+                            0,
+                            this.width,
+                            this.height);
+                    this.tiles.set(name, buffer);
+                }
+
+                draw(name, context, x, y) {
+                    const buffer = this.tiles.get(name);
+                    context.drawImage(buffer, x, y);
+                }
+
+                drawTile(name, context, x, y) {
+                    this.draw(name, context, x * this.width, y * this.height);
+                }
+            };
+
+            loadImage('https://jilijo01.github.io/tiles.png')
+                .then(image => {
+                    const sprites = new SpriteSheet(image, 16, 16);
+                    sprites.define('ground', 0, 0);
+                    sprites.define('sky', 3, 23);
+                    sprites.draw('sky', context, 180, 162);
+                    for (let x = 0; x < 25; ++x) {
+                        for (let y = 0; y < 14; ++y) {
+                            sprites.drawTile('sky', context, x, y);
+                        }
+                    }
+                    for (let x = 0; x < 25; ++x) {
+                        for (let y = 12; y < 14; ++y) {
+                            sprites.drawTile('ground', context, x, y);
+                        }
+                    }
+                });
         }
-        
 
         //Fired when the widget is added to the html DOM of the page
         connectedCallback() {
