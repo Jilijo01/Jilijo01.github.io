8ab3ddc254198be407c48d8c360f769bf64f92b0
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-07-24 07:22:38 +0200

    Update mario.js

--- mario.js
+++ mario.js
@@ -68,13 +68,13 @@
             }
 
             function drawBackground(background, context, sprites){
-                background.ranges.forEach([x1,x2,y1.y2]) => {
+                background.ranges.foreach(([x1,x2,y1.y2]) => {
                     for (let x = x1; x < x2; ++x) {
                         for (let y = y1; y < y2; ++y) {
                             sprites.drawTile(background.tile, context, x, y);
                         }
                     }
-                }
+                });
             }
 
             loadImage('https://jilijo01.github.io/tiles.png')
