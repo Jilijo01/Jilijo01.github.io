cf363df3e03491696a3612f09b577e706075026a
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-07-24 07:24:48 +0200

    Update mario.js

--- mario.js
+++ mario.js
@@ -68,7 +68,7 @@
             }
 
             function drawBackground(background, context, sprites){
-                background.ranges.foreach(([x1,x2,y1,y2]) => {
+                background.ranges.forEach(([x1,x2,y1,y2]) => {
                     for (let x = x1; x < x2; ++x) {
                         for (let y = y1; y < y2; ++y) {
                             sprites.drawTile(background.tile, context, x, y);
