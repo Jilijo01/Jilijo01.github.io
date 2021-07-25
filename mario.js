230ab75e4903f77a9bf1f73868d869bc56bf2152
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-07-25 14:39:00 +0200

    Update mario.js

--- mario.js
+++ mario.js
@@ -1,6 +1,3 @@
-
-
-
 (function () {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
@@ -17,60 +14,48 @@
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
             this._firstConnection = false;
             var context = this._shadowRoot.getElementById('screen').getContext('2d');
-            function loadImage(url) {
-                return new Promise(resolve => {
-                    const image = new Image();
-                    image.addEventListener('load', () => {
-                        resolve(image);
-                    });
-                    image.src = url;
-                });
-            }
+            };
 
 
 
-            //Fired when the widget is added to the html DOM of the page
-            connectedCallback() {
-                this._firstConnection = true;
-                this.redraw();
-            }
 
-            //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
-            disconnectedCallback() {
+//Fired when the widget is added to the html DOM of the page
+connectedCallback() {
+    this._firstConnection = true;
+    this.redraw();
+}
 
-            }
+//Fired when the widget is removed from the html DOM of the page (e.g. by hide)
+disconnectedCallback() {
 
-            //When the custom widget is updated, the Custom Widget SDK framework executes this function first
-            onCustomWidgetBeforeUpdate(oChangedProperties) {
+}
 
-            }
+//When the custom widget is updated, the Custom Widget SDK framework executes this function first
+onCustomWidgetBeforeUpdate(oChangedProperties) {
 
-            //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
-            onCustomWidgetAfterUpdate(oChangedProperties) {
-                if (this._firstConnection) {
-                    this.redraw();
-                }
-            }
+}
 
-            //When the custom widget is removed from the canvas or the analytic application is closed
-            onCustomWidgetDestroy() {
-            }
+//When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
+onCustomWidgetAfterUpdate(oChangedProperties) {
+    if (this._firstConnection) {
+        this.redraw();
+    }
+}
 
-
-            //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
-            // Commented out by default.  If it is enabled, SAP Analytics Cloud will track DOM size changes and call this callback as needed
-            //  If you don't need to react to resizes, you can save CPU by leaving it uncommented.
-            /*
-            onCustomWidgetResize(width, height){
-                redraw()
-            }
-            */
-
-            redraw() {
-            }
+//When the custom widget is removed from the canvas or the analytic application is closed
+onCustomWidgetDestroy() {
+}
 
 
-        }
-    })
+//When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
+// Commented out by default.  If it is enabled, SAP Analytics Cloud will track DOM size changes and call this callback as needed
+//  If you don't need to react to resizes, you can save CPU by leaving it uncommented.
+/*
+onCustomWidgetResize(width, height){
+    redraw()
+}
+*/
 
-})
+redraw() {}
+    });
+}) ();
