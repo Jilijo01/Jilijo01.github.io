892508e563f2911000e0a6a0f71859310d8bb7e6
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-07-25 08:12:12 +0200

    Update mario.js

--- mario.js
+++ mario.js
@@ -126,7 +126,7 @@
 
             function createSpriteLayer(sprite, pos){
                 return function drawSpritesLayer(context){
-                    for(let i = 0, i < 20, i++){\
+                    for(let i = 0, i < 20, i++){
                         sprite.draw('idle', context, pos.x, pos.y);
                     }
                     
