b6a926ef55d22ec5004e5e31c7c55feb867944a8
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-07-26 07:48:52 +0200

    Update mario.js

--- mario.js
+++ mario.js
@@ -201,7 +201,7 @@
                     const comp = new Compositor();
                     const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
                     comp.layers.push(backgroundLayer);
-                    const gravity = 30;
+                    const gravity = 2000;
                     mario.pos.set(64, 180);
                     mario.vel.set(200, -600);
                     const spriteLayer = createSpriteLayer(mario);
@@ -209,9 +209,9 @@
 
                     const timer = new Timer(1 / 60);
                     timer.update = function update(deltaTime){
-                        comp.draw(context);
                         mario.update(deltaTime);
-                        mario.vel.y += gravity;
+                        comp.draw(context);
+                        mario.vel.y += gravity * deltaTime;
                     }
                 timer.start(); 
                     
