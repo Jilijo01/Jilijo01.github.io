3fb72d9e34d9125efbc5383d0936412b36a2affb
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-07-26 15:28:35 +0200

    Update mario.js

--- mario.js
+++ mario.js
@@ -60,7 +60,7 @@
             //Timer.js
 
             //math.js
-            class Vec2 {
+             class Vec2 {
                 constructor(x, y) {
                     this.set(x, y);
                 }
@@ -161,6 +161,49 @@
                 }
             }
             //velocity.js
+            
+            //spritesheet.js
+            class SpriteSheet {
+                constructor(image, width, height) {
+                    this.image = image;
+                    this.width = width;
+                    this.height = height;
+                    this.tiles = new Map();
+                }
+            
+                define(name, x, y, width, height) {
+                    const buffer = document.createElement('canvas');
+                    buffer.width = width;
+                    buffer.height = height;
+                    buffer
+                        .getContext('2d')
+                        .drawImage(
+                            this.image,
+                            x,
+                            y,
+                            width,
+                            height,
+                            0,
+                            0,
+                            width,
+                            height);
+                    this.tiles.set(name, buffer)
+                }
+            
+                defineTile(name, x, y) {
+                    this.define(name, x * this.width, y * this.height, this.width, this.height);
+                }
+            
+                draw(name, context, x, y) {
+                    const buffer = this.tiles.get(name);
+                    context.drawImage(buffer, x, y);
+                }
+            
+                drawTile(name, context, x, y) {
+                    this.draw(name, context, x * this.width, y * this.height);
+                }
+            }
+            //spritesheet.js
 
             //sprites.js
             function loadMarioSprite() {
