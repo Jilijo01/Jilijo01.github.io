7d2a7e78e23bc00113727213c4493014cde19c02
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-07-26 14:56:22 +0200

    Update mario.js

--- mario.js
+++ mario.js
@@ -190,7 +190,7 @@
                 }
             }
 
-            class entity {
+            class Entity {
                 constructor() {
                     this.pos = new Vec2(0, 0);
                     this.vel = new Vec2(0, 0);
