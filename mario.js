b1c9b8bbe12eb33b6573cf12657a66a9dc5c2bc8
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-07-26 14:48:58 +0200

    Update mario.js

--- mario.js
+++ mario.js
@@ -286,37 +286,39 @@
             ])
                 .then(([mario, backgroundSprites, level]) => {
                     const comp = new Compositor();
+
                     const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
                     comp.layers.push(backgroundLayer);
-                    
+                
                     const gravity = 2000;
-                    const SPACE = 32;
-                    mario.vel.set(200, -600);
                     mario.pos.set(64, 180);
-                    
-                    
-                    const input = new KeyboardState();
-                    input.addMapping(SPACE, keyState =>{
-                        if (keyState){
+                
+                
+                    const SPACE = 32;
+                    const input = new Keyboard();
+                    input.addMapping(SPACE, keyState => {
+                        if (keyState) {
                             mario.jump.start();
                         } else {
                             mario.jump.cancel();
                         }
-                        console.log(keyState);
                     });
                     input.listenTo(window);
-
-
+                
+                
                     const spriteLayer = createSpriteLayer(mario);
                     comp.layers.push(spriteLayer);
-
-                    const timer = new Timer(1 / 60);
-                    timer.update = function update(deltaTime){
+                
+                    const timer = new Timer(1/60);
+                    timer.update = function update(deltaTime) {
                         mario.update(deltaTime);
+                
                         comp.draw(context);
+                
                         mario.vel.y += gravity * deltaTime;
                     }
-                timer.start(); 
+                
+                    timer.start();
                     
                 }); 
 
