b2e02d1bbe795ffcb639364d54b27f95dd7af107
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-07-26 08:38:43 +0200

    Update mario.js

--- mario.js
+++ mario.js
@@ -195,6 +195,30 @@
                 }
             }
 
+            class KeyboardState(){
+                constructor(){
+                    // holds the current state of a given key
+                    this.keyStates = new Map();
+                    // holds callback functions for a key code
+                    this.keyMap = new Map();
+                }
+                addMapping(keyCode, callback){
+                    this.keyMap.set(keyCode, callback);
+                }
+
+                handleEvent(event){
+                    const {keyCode} = event;
+
+                    if(!this.keyMap.has(keyCode)){
+                        // Did not have key mapped
+                        return false;
+                    }
+
+                    event.preventDefault();
+                }
+            }
+
+
             Promise.all([
                 createMario(),
                 loadBackgroundSprites(),
