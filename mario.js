c4dd2c52bbb39b98283a8810d6e3984944030f17
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-07-26 12:14:29 +0200

    Update mario.js

--- mario.js
+++ mario.js
@@ -178,6 +178,39 @@
 
                 }
             }
+
+             // Entity.js
+             class Trait {
+                constructor(name){
+                    this.NAME = name;
+                }
+
+                update(){
+                    console.warn('Unhandled update call in Trait');
+                }
+            }
+
+            class entity {
+                constructor() {
+                    this.pos = new Vec2(0, 0);
+                    this.vel = new Vec2(0, 0);
+
+                    this.traits = [];
+                }
+
+                addTrait(trait){
+                    this.traits.push(trait)
+                    this[trait.NAME] = trait;
+                }
+
+                update(deltaTime){
+                    this.traits.forEach(trait =>{
+                        trait.update(this, deltaTime);
+                    });
+                }
+            }
+            // Entity.js
+
             //entities.js
 
             class Velocity {
@@ -218,37 +251,7 @@
             }
             //math.js
 
-            // Entity.js
-            class Trait {
-                constructor(name){
-                    this.NAME = name;
-                }
-
-                update(){
-                    console.warn('Unhandled update call in Trait');
-                }
-            }
-
-            class entity {
-                constructor() {
-                    this.pos = new Vec2(0, 0);
-                    this.vel = new Vec2(0, 0);
-
-                    this.traits = [];
-                }
-
-                addTrait(trait){
-                    this.traits.push(trait)
-                    this[trait.NAME] = trait;
-                }
-
-                update(deltaTime){
-                    this.traits.forEach(trait =>{
-                        trait.update(this, deltaTime);
-                    });
-                }
-            }
-            // Entity.js
+           
 
             class Timer {
                 constructor(deltaTime = 1 / 60) {
