b49d7fe2d8d5e1198065a9c5ae62343608cb60e5
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-08-04 23:41:20 +0200

    Update CSV.js

--- CSV.js
+++ CSV.js
@@ -5,7 +5,7 @@
     <canvas id="screen" width="640" height="640"></canvas>
         
     `;
-    customElements.define('com-sap-sample-helloworld1', class HelloWorld1 extends HTMLElement {
+    customElements.define('com-sap-sample-helloworld1', class CSV_INPUT extends HTMLElement {
 
 
         constructor() {
