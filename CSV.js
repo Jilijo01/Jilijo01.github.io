adf1697148ce0e03bfc0e3b82036e620cdc0a6de
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-08-05 07:34:09 +0200

    Add files via upload

---
+++ CSV.js
@@ -0,0 +1,74 @@
+(function () {
+    let tmpl = document.createElement('template');
+    tmpl.innerHTML = `
+    <script type=""text/javascript" src="https://raw.githubusercontent.com/mholt/PapaParse/master/papaparse.js"></script> 
+    <title>READ CSV</title>
+    <input type="file" id="upload-csv" accept=".csv"></input>
+    <button id="btn-upload-csv">Read CSV</button>
+    <table id="tbl-data"></table>
+    `;
+
+    customElements.define('com-sap-sample-helloworld2', class HelloWorld1 extends HTMLElement {
+
+
+        constructor() {
+            super();
+            this._shadowRoot = this.attachShadow({ mode: "open" });
+            this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            this._firstConnection = false;
+	    let btn_upload = document.getElementById('btn-upload-csv').addEventListener('click', () => {
+            Papa.parse(document.getElementById('upload-csv').files[0],{
+            download: true,
+            header: false,
+            complete: function(results){
+                console.log(results);
+            }
+        }
+        )
+    })
+
+        }
+
+        //Fired when the widget is added to the html DOM of the page
+        connectedCallback() {
+            this._firstConnection = true;
+            this.redraw();
+        }
+
+        //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
+        disconnectedCallback() {
+
+        }
+
+        //When the custom widget is updated, the Custom Widget SDK framework executes this function first
+        onCustomWidgetBeforeUpdate(oChangedProperties) {
+
+        }
+
+        //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
+        onCustomWidgetAfterUpdate(oChangedProperties) {
+            if (this._firstConnection) {
+                this.redraw();
+            }
+        }
+
+        //When the custom widget is removed from the canvas or the analytic application is closed
+        onCustomWidgetDestroy() {
+
+        }
+
+
+        //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
+        // Commented out by default
+        /*
+        onCustomWidgetResize(width, height){
+        
+        }
+        */
+
+        redraw() { }
+
+
+    });
+
+})();
