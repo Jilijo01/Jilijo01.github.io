63817a737c576ee19071d7de4e1e48643e31688a
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-08-21 07:27:34 +0200

    Update CSV.js

--- CSV.js
+++ CSV.js
@@ -1,11 +1,11 @@
 (function () {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
-    <script type="text/javascript" src="papaparse.min.js"></script> 
-    <title>READ CSV</title>
-    <input type="file" id="upload-csv" accept=".csv"></input>
-    <button id="btn-upload-csv">Read CSV</button>
-    <table id="tbl-data"></table>
+    <input type="file" id="fileUpload" />
+    <input type="button" id="upload" value="Upload" onclick="Upload()" />
+    <hr />
+    <div id="dvCSV">
+    </div>
     `;
 
     customElements.define('com-sap-sample-helloworld2', class HelloWorld1 extends HTMLElement {
@@ -16,47 +16,47 @@
             this._shadowRoot = this.attachShadow({ mode: "open" });
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
             this._firstConnection = false;
-	    /*let btn_upload = this._shadowRoot.getElementById('btn-upload-csv').addEventListener('click', () => {
-            Papa.parse(this._shadowRoot.getElementById('upload-csv').files[0],{
-            download: true,
-            header: false,
-            complete: function(results){
-                console.log(results);
-            }
-        }
-        )
-    })*/ 
-	function Upload() {
-        var fileUpload = document.getElementById("fileUpload");
-        var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.csv|.txt)$/;
-        if (regex.test(fileUpload.value.toLowerCase())) {
-            if (typeof (FileReader) != "undefined") {
-                var reader = new FileReader();
-                reader.onload = function (e) {
-                    var table = document.createElement("table");
-                    var rows = e.target.result.split("\n");
-                    for (var i = 0; i < rows.length; i++) {
-                        var cells = rows[i].split(",");
-                        if (cells.length > 1) {
-                            var row = table.insertRow(-1);
-                            for (var j = 0; j < cells.length; j++) {
-                                var cell = row.insertCell(-1);
-                                cell.innerHTML = cells[j];
-                            }
-                        }
-                    }
-                    var dvCSV = document.getElementById("dvCSV");
-                    dvCSV.innerHTML = "";
-                    dvCSV.appendChild(table);
+            /*let btn_upload = this._shadowRoot.getElementById('btn-upload-csv').addEventListener('click', () => {
+                Papa.parse(this._shadowRoot.getElementById('upload-csv').files[0],{
+                download: true,
+                header: false,
+                complete: function(results){
+                    console.log(results);
                 }
-                reader.readAsText(fileUpload.files[0]);
-            } else {
-                alert("This browser does not support HTML5.");
             }
-        } else {
-            alert("Please upload a valid CSV file.");
-        }
-    }
+            )
+        })*/
+            function Upload() {
+                var fileUpload = this._shadowRoot.getElementById("fileUpload");
+                var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.csv|.txt)$/;
+                if (regex.test(fileUpload.value.toLowerCase())) {
+                    if (typeof (FileReader) != "undefined") {
+                        var reader = new FileReader();
+                        reader.onload = function (e) {
+                            var table = this._shadowRoot.createElement("table");
+                            var rows = e.target.result.split("\n");
+                            for (var i = 0; i < rows.length; i++) {
+                                var cells = rows[i].split(",");
+                                if (cells.length > 1) {
+                                    var row = table.insertRow(-1);
+                                    for (var j = 0; j < cells.length; j++) {
+                                        var cell = row.insertCell(-1);
+                                        cell.innerHTML = cells[j];
+                                    }
+                                }
+                            }
+                            var dvCSV = this._shadowRoot.getElementById("dvCSV");
+                            dvCSV.innerHTML = "";
+                            dvCSV.appendChild(table);
+                        }
+                        reader.readAsText(fileUpload.files[0]);
+                    } else {
+                        alert("This browser does not support HTML5.");
+                    }
+                } else {
+                    alert("Please upload a valid CSV file.");
+                }
+            }
         }
 
         //Fired when the widget is added to the html DOM of the page
