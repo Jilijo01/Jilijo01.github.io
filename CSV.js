87158dc8316eb1a9d2aafee7e0d8829fc535f1e0
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-08-21 07:05:45 +0200

    Update CSV.js

--- CSV.js
+++ CSV.js
@@ -16,8 +16,8 @@
             this._shadowRoot = this.attachShadow({ mode: "open" });
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
             this._firstConnection = false;
-	    /*let btn_upload = document.getElementById('btn-upload-csv').addEventListener('click', () => {
-            Papa.parse(document.getElementById('upload-csv').files[0],{
+	    let btn_upload = document.getElementById('btn-upload-csv').addEventListener('click', () => { console.log("CLICK")};
+            /*Papa.parse(document.getElementById('upload-csv').files[0],{
             download: true,
             header: false,
             complete: function(results){
