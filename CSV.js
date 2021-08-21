49d6b99abb25b5dc65123273a012c0ae53b0b930
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-08-21 07:24:25 +0200

    Update CSV.js

--- CSV.js
+++ CSV.js
@@ -16,7 +16,7 @@
             this._shadowRoot = this.attachShadow({ mode: "open" });
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
             this._firstConnection = false;
-	    let btn_upload = this._shadowRoot.getElementById('btn-upload-csv').addEventListener('click', () => {
+	    /*let btn_upload = this._shadowRoot.getElementById('btn-upload-csv').addEventListener('click', () => {
             Papa.parse(this._shadowRoot.getElementById('upload-csv').files[0],{
             download: true,
             header: false,
@@ -25,7 +25,38 @@
             }
         }
         )
-    })
+    })*/ 
+	function Upload() {
+        var fileUpload = document.getElementById("fileUpload");
+        var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.csv|.txt)$/;
+        if (regex.test(fileUpload.value.toLowerCase())) {
+            if (typeof (FileReader) != "undefined") {
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    var table = document.createElement("table");
+                    var rows = e.target.result.split("\n");
+                    for (var i = 0; i < rows.length; i++) {
+                        var cells = rows[i].split(",");
+                        if (cells.length > 1) {
+                            var row = table.insertRow(-1);
+                            for (var j = 0; j < cells.length; j++) {
+                                var cell = row.insertCell(-1);
+                                cell.innerHTML = cells[j];
+                            }
+                        }
+                    }
+                    var dvCSV = document.getElementById("dvCSV");
+                    dvCSV.innerHTML = "";
+                    dvCSV.appendChild(table);
+                }
+                reader.readAsText(fileUpload.files[0]);
+            } else {
+                alert("This browser does not support HTML5.");
+            }
+        } else {
+            alert("Please upload a valid CSV file.");
+        }
+    }
         }
 
         //Fired when the widget is added to the html DOM of the page
