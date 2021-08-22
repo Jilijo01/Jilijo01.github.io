53ef4f8afdead64faf9d2be2bd299a346220c5af
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-08-22 07:57:23 +0200

    Update CSV.js

--- CSV.js
+++ CSV.js
@@ -73,11 +73,19 @@
                 // return the array
                 return arr;
               }
-              reader.onload = function (e) {
-                const text = e.target.result;
-                const data = csvToArray(text);
-                document.write(JSON.stringify(data));
-              };
+              myForm.addEventListener("submit", function (e) {
+                e.preventDefault();
+                const input = csvFile.files[0];
+                const reader = new FileReader();
+          
+                reader.onload = function (e) {
+                  const text = e.target.result;
+                  const data = csvToArray(text);
+                  document.write(JSON.stringify(data));
+                };
+                
+                reader.readAsText(input);
+              });
         }
 
         //Fired when the widget is added to the html DOM of the page
