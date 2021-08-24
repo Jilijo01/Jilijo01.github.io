a3379b37d0796f4d7958f16265e039752d0fce87
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-08-24 07:58:56 +0200

    Update CSV.js

--- CSV.js
+++ CSV.js
@@ -90,7 +90,7 @@
           
                 reader.onload = function (e) {
                   const text = e.target.result;
-                  const data = csvToArray(text);
+                  this.data = csvToArray(text);
                   //document.write(JSON.stringify(data));
                   console.log(data);
                 };
