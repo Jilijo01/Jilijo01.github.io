195211201f6c825fe64cf70c782ec11faae26e69
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-08-24 08:13:36 +0200

    Update CSV.js

--- CSV.js
+++ CSV.js
@@ -16,7 +16,8 @@
             this._shadowRoot = this.attachShadow({ mode: "open" });
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
             this._firstConnection = false;
-            this._data = [];
+            var data = [];
+            this._shadowRoot.appendChild(data);
 
             const myForm = this._shadowRoot.getElementById("myForm");
             const csvFile = this._shadowRoot.getElementById("csvFile");
