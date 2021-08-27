df775b169d7a32af6791878eb1f89747ce56b5a0
Jilijo01 <Jonathan.ilijortas@gmail.com>
2021-08-27 17:11:37 +0200

    Update CSV.js

--- CSV.js
+++ CSV.js
@@ -28,7 +28,8 @@
                 const input = csvFile.files[0];
                 const reader = new FileReader();
                 reader.onload = function (event) {
-                    console.log(event.target.result); // the CSV content as string
+                    reader.readAsText(input);
+
                   };
               });
 
